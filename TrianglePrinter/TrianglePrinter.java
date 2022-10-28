e69954395406bdd742a7c569cf612ffccfea5510
Hiroten31 <hiroten31@gmail.com>
2022-10-28 15:32:57 +0100

    Now prints for even and odd numbers >2.

--- TrianglePrinter/TrianglePrinter.java
+++ TrianglePrinter/TrianglePrinter.java
@@ -3,51 +3,64 @@ import java.util.Scanner;
 public class TrianglePrinter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter length of bottom line of the triangle: ");
+        System.out.println("Enter integer of bottom line of the triangle: ");
         int x = sc.nextInt();
         //Check if 'x' is a good input
-        while(x <= 0){
-            System.out.println("Enter length greater than 0: ");
+        while(x <= 2){
+            System.out.println("Enter integer greater than 2: ");
             x = sc.nextInt();
         }
         PrintingTriangle(x);
         sc.close();
     }
     static void PrintingTriangle(int length){
-        //First line, the tip
-        for(int j = length+1; j > 0; j--){
-            System.out.print(" ");
-        }
-        System.out.print("*");
-        for(int j = length+1; j > 0; j--){
-            System.out.print(" ");
-        }
-        System.out.println("|");
-        //Rest of the triangle
-        for(int i = length; i > 0; i--){
-            //Left spacing
-            for(int j = i; j > 0; j--){
-                System.out.print(" ");
-            }
-            //Left line
+        //Check if number is odd or even. It will determine tip of the triangle and futher placement
+        if(length%2 == 1){
+            //Triangle's tip
+            Spacing((length-1)/2);
             System.out.print("*");
-            //Inside
-            for(int j = 1+(length - i)*2; j > 0; j--){
-                System.out.print(" ");
-            }
-            //Right line
-            System.out.print("*");
-            //Right spacing
-            for(int j = i; j > 0; j--){
-                System.out.print(" ");
-            }
+            Spacing((length-1)/2);
             System.out.println("|");
+            //Triangle's walls
+            for(int i = ((length-1)/2)-1; i > 0; i--){
+                Spacing(i);
+                //Left wall
+                System.out.print("*");
+                //Middle space
+                Spacing(length - ((i+1)*2));
+                //Right wall
+                System.out.print("*");
+                Spacing(i);
+                System.out.println("|");
+            }
+        } else {
+            //Triangle's tip
+            Spacing((length/2)-1);
+            System.out.print("**");
+            Spacing((length/2)-1);
+            System.out.println("|");
+            for(int i = ((length/2)-2); i > 0; i--){
+                Spacing(i);
+                //Left wall
+                System.out.print("*");
+                //Middle space
+                Spacing(length - (i*2)-2);
+                //Right wall
+                System.out.print("*");
+                Spacing(i);
+                System.out.println("|");
+            }
         }
         //Bottom line
-        for(int j = (length)*2+3; j > 0; j--){
+        for(int j = length; j > 0; j--){
             System.out.print("*");
         }
         System.out.println("|");
     }
+    //A function to not repeat one loop multiple times
+    static void Spacing(int times){
+        for(int i = times; i > 0; i--){
+            System.out.print(" ");
+        }
+    }
 }
- 
