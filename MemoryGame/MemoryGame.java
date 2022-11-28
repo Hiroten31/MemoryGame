a5322e451251637162bf97138e64b16fc8dcc51a
Hiroten31 <hiroten31@gmail.com>
2022-11-28 15:12:00 +0100

    Moved list initialization, fixed matrix printing

--- MemoryGame/MemoryGame.java
+++ MemoryGame/MemoryGame.java
@@ -8,40 +8,48 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MemoryGame {
+    static ArrayList<String> wordsList = new ArrayList<String>();
+    static ArrayList<String> pickedWords = new ArrayList<String>();
+    //Method to clear console out of useless stuff.
     public static void consoleClear(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
     
+    //Method to pick up random words for the game.
     public static void pickRandomWords(int rows, int collumns) throws IOException{
         //Here you can choose from which file you want to take words.
         File file = new File("MemoryGame/words.txt");
 
+        //Adding every line of choosen file to wordsList.
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-        ArrayList<String> wordsList = new ArrayList<String>();
+        
         try {
             String line;
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null)
                 wordsList.add(line);
-            }
         } finally {
             br.close();
         }
+
+        //Picking up random index for wordsList to add indexed word to pickedWords list.
         Random random = new Random();
-        ArrayList<String> pickedWords = new ArrayList<String>();
+        
         for(int i = 0; i < ((rows*collumns)-((rows*collumns)%2))/2; i++) {
             int wordIndex = random.nextInt(wordsList.size());
             pickedWords.add(i, wordsList.get(wordIndex));
             wordsList.remove(wordIndex);
         }
+
+        //Information about what words have been choosen for the game.
         System.out.println("\n\n\nPICKED WORDS ARE: \n");
-        for(String word : pickedWords){
+        for(String word : pickedWords)
             System.out.print(word + ", ");
-        }
+
+        //Reusing the old list to make it full of 'x' for printing them into the matrix.
         wordsList.clear();
-        for(int j = 0; j < pickedWords.size(); j++){
+        for(int j = 0; j < pickedWords.size()*2; j++) 
             wordsList.add(j, "x");
-        }
     }
     
     public static void main(String[] args) throws IOException {
@@ -63,15 +71,20 @@ public class MemoryGame {
             pickRandomWords(rows, collumns);
             
             hearths = ((rows*collumns) + ((rows * collumns)%2))/2;
-            System.out.println("\n\n\t\t--- MEMORY GAME ---\nHearths: " + hearths + "\n\t");
+            System.out.println("\n\n\t\t--- MEMORY GAME ---\nHearths: " + hearths + "\n\n");
             for(int i = 1; i <= collumns; i++) {
                 System.out.print("\t\t" + i);
             }
+            int x = 0;
             for(int i = 1; i <= rows; i++) {
-                System.out.println("\n" + i + ".\telo eloelo elo\t\tx\t\tx");
+                System.out.print("\n" + i + ". ");
+                for(int j = 0; j < collumns; j++){
+                    System.out.print("\t\t" + wordsList.get(x));
+                    x++;
+                }
             }
         }
-        
+
         sc.close();
     }
 }
