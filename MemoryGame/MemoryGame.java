01b9719feb4db3000e98d4fe13e55f1a67b3ccc8
Hiroten <Hiroten31@gmail.com>
2022-11-30 14:25:34 +0100

    Moved some methods, fixed matrix for odd numbers

--- MemoryGame/MemoryGame.java
+++ MemoryGame/MemoryGame.java
@@ -1,5 +1,6 @@
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 import java.io.BufferedReader;
@@ -10,7 +11,9 @@ import java.io.IOException;
 public class MemoryGame {
     static ArrayList<String> wordsList = new ArrayList<String>();
     static ArrayList<String> pickedWords = new ArrayList<String>();
+    static ArrayList<String> wordsToUncover = new ArrayList<String>();
     static int hearths;
+
     //Method to clear console out of useless stuff.
     public static void consoleClear(){
         System.out.print("\033[H\033[2J");
@@ -24,7 +27,6 @@ public class MemoryGame {
 
         //Adding every line of choosen file to wordsList.
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-        
         try {
             String line;
             while ((line = br.readLine()) != null)
@@ -33,7 +35,7 @@ public class MemoryGame {
             br.close();
         }
 
-        //Picking up random index for wordsList to add indexed word to pickedWords list.
+        //Picking up random index from wordsList to add indexed word to pickedWords list.
         Random random = new Random();
         
         for(int i = 0; i < ((rows*collumns)-((rows*collumns)%2))/2; i++) {
@@ -42,49 +44,57 @@ public class MemoryGame {
             wordsList.remove(wordIndex);
         }
 
-        //Information about what words have been choosen for the game.
-        System.out.println("\n\n\nPICKED WORDS ARE: \n");
-        for(String word : pickedWords)
-            System.out.print(word + ", ");
-
         //Reusing the old list to make it full of 'x' for printing them into the matrix.
         wordsList.clear();
         for(int j = 0; j < pickedWords.size()*2; j++) 
             wordsList.add(j, "x");
+        //Filling list of words from which we will pick words in matrix
+        wordsToUncover.addAll(pickedWords);
+        wordsToUncover.addAll(pickedWords);
+        Collections.shuffle(wordsToUncover);
+        if((rows*collumns)%2 == 1){
+            wordsList.add(0, "EMPTY");
+            wordsToUncover.add(0, "EMPTY");
+        }
     }
     
     public static void printMatrix(int r, int c, int coords){
-        System.out.println("\n\n\t\t--- MEMORY GAME ---\nHearths: " + hearths + "\n\n");
+        System.out.println("\n\n\t\t--- MEMORY GAME ---\nHearths: " + hearths + "\n");
+
+        //Information about what words have been choosen for the game.
+        System.out.println("\n\nPICKED WORDS ARE: ");
+        for(int k = 0; k < pickedWords.size()-1; k++)
+            System.out.print(pickedWords.get(k) + ", ");
+        System.out.print(pickedWords.get(pickedWords.size()-1) + ".\n\n");
+
+        //Printing table
         for(int i = 1; i <= c; i++) {
             System.out.print("\t\t" + i);
         }
         int x = 0;
-        for(int i = 1; i <= r; i++) {
-            System.out.print("\n" + i + ". ");
-            for(int j = 0; j < c; j++){
-                System.out.print("\t\t" + wordsList.get(x));
-                x++;
-            }
-        }
-        if(coords != 0){
-            System.out.println("\n\n\t\t--- MEMORY GAME ---\nHearths: " + hearths + "\n\n");
-            for(int i = 1; i <= c; i++) {
-                System.out.print("\t\t" + i);
-            }
-            x = 0;
+        if(coords != -1){
             for(int i = 1; i <= r; i++) {
                 System.out.print("\n" + i + ". ");
                 for(int j = 0; j < c; j++){
                     if(x == coords){
-                        System.out.print("\t\t" + pickedWords.get(x));
+                        System.out.print("\t\t" + wordsToUncover.get(x));
                     } else {
                         System.out.print("\t\t" + wordsList.get(x));
                     }
                     x++;
                 }
             }
+        } else {
+            for(int i = 1; i <= r; i++) {
+                System.out.print("\n" + i + ". ");
+                for(int j = 0; j < c; j++){
+                    System.out.print("\t\t" + wordsList.get(x));
+                    x++;
+                }
+            }
         }
     }
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- MEMORY GAME ---\nChoose: \n1. Start game\n2. Highscores\n3. Exit\n\nPress defined key to do the action...");
@@ -104,24 +114,26 @@ public class MemoryGame {
             pickRandomWords(rows, collumns);
             
             hearths = ((rows*collumns) + ((rows * collumns)%2))/2;
-            int coordinates = 0;
+            int coordinates = -1;
             printMatrix(rows, collumns, coordinates);
             do {
-                System.out.println("\nGive me coordinates of respectively rows and collumns you want to uncover: \n");
-                int uncoverRow = sc.nextInt();
-                int uncoverCollumn = sc.nextInt();
+                int uncoverRow, uncoverCollumn;
+                System.out.print("\nGive me coordinates in the range of the matrix to uncover choosen word: \nRow: ");
+                do{
+                    uncoverRow = sc.nextInt();
+                } while(uncoverRow >= rows && uncoverRow < 0);
+                System.out.print("Collumn: ");
+                do{
+                    uncoverCollumn = sc.nextInt();
+                } while(uncoverCollumn >= collumns && uncoverCollumn < 0);
                 coordinates = (uncoverRow-1)*collumns + uncoverCollumn-1;
                 printMatrix(rows, collumns, coordinates);
             } while(hearths > 0);
-            
-
         }
 
         sc.close();
     }
 }
 /*To do:
-- Duplicating words in pickedWords list.
-- Shuffling them randomly.
 - Saving the choosen coordinates and then comparing if another ones are the same (perhaps coordinates2 in method?).
 */
